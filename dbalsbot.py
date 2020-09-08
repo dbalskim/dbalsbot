c6dcdc62bb3c2b597e53bae28a831855a4f15fcc
dbalskim <70954150+dbalskim@users.noreply.github.com>
2020-09-08 22:27:48 +0900

    Update dbalsbot.py

--- dbalsbot.py
+++ dbalsbot.py
@@ -28,6 +28,12 @@ async def on_message(message):
             await message.channel.send("ㅗ")
     if message.content.endswith("잘했어"):
         await message.channel.send("감사합니다")
+    if message.content.endswith("드발스"):
+        await message.channel.send("뭐")
+    if message.content.endswith("개발련"):
+        await message.channel.send("응 닥쳐")
+    if message.content.endswith("바보"):
+        await message.channel.send("김연성 킥한다 ㅅㄱ")
     if message.content.endswith("나 잘생김?"):
         await message.channel.send("ㄴ")
     if message.content.endswith("잘생김"):
@@ -43,7 +49,7 @@ async def on_message(message):
     if message.content.endswith("김연성은?"):
         await message.channel.send("바보")
     if message.content.endswith("이동현은?"):
-        await message.channel.send("공항동둑")
+        await message.channel.send("블랙스컬")
     if message.content.endswith("김태연은?"):
         await message.channel.send("난 한국인이야~")
     if message.content.endswith("김경빈은?"):
@@ -82,26 +88,26 @@ async def on_message(message):
         author = message.guild.get_member(int(message.content[0:18]))
         role = discord.utils.get(message.guild.roles, name="Muted")
         await author.add_roles(role)
+        await message.channel.send("뮤트되었습니다.")
 
     if message.content.endswith("뮤트해제"):
         author = message.guild.get_member(int(message.content[0:18]))
         role = discord.utils.get(message.guild.roles, name="Muted")
         await author.remove_roles(role)
+        await message.channel.send("뮤트가 해제되었습니다.")
 
     if message.content.endswith("사진 목록"):
-        files = os.listdir("C:\\김유민 폴더\\파이참\\dbalsbot")
-        for f in files:
-            if f.endswith(".png"):
-                await client.get_channel(726374006701293578).send(f)
+        await message.channel.send("공지사항을 참고하세요.")
     if message.content.startswith("도움말"):
-        await message.channel.send("호출: \"하이 드발스\"")
-        await message.channel.send("칭찬: \"잘했어\"")
-        await message.channel.send("사진보내기: \"사진이름.png 사진 보내줘\"")
-        await message.channel.send("사진목록 확인: \"사진 목록\"")
-        await message.channel.send("특정 채널에 메시지: \"채널ID 내용 보내줘\"")
-        await message.channel.send("개인 메시지: \"/DM 유저ID 내용\"")
-        await message.channel.send("뮤트: \"유저ID 저새기 뮤트좀\"")
-        await message.channel.send("언뮤트: \"유저ID 뮤트해제\"")
+        await message.channel.send("""호출: \"하이 드발스\"
+칭찬: \"잘했어\"
+물어보기: \"이름은/는?\"
+사진보내기: \"사진이름.png 사진 보내줘\"
+사진목록 확인: \"사진 목록\"
+특정 채널에 메시지: \"채널ID 내용 보내줘\"
+개인 메시지: \"/DM 유저ID 내용\"
+뮤트: \"유저ID 저새기 뮤트좀\"
+언뮤트: \"유저ID 뮤트해제\"""")
 
 
 access_token = os.environ["BOT_TOKEN"]
